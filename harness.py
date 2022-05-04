82c7c27611a4888722319391080d636c4b84cc2d
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2022-05-04 09:29:47 -0700

    Add failing `SLT` test.

--- harness.py
+++ harness.py
@@ -202,6 +202,21 @@ class TestSetOnLessThan:
         expect_memory(go.inspect_mem(cpu.rf), {t0: 0, t1: 7, t2: 5})
         expect_memory(go.inspect_mem(cpu.d_mem), {})
 
+    # def test_slt_with_negative_second_operand_and_destination_is_not_operand(self):
+    #     memory = {
+    #         cpu.rf:    {t1: 5, t2: -7},
+    #         cpu.i_mem: {0: 0x012A402A}
+    #     }
+
+    #     go = cpu.Simulation(
+    #         register_value_map = {cpu.pc: 0},
+    #         memory_value_map = memory
+    #     )
+    #     
+    #     go.step({})
+    #     expect_memory(go.inspect_mem(cpu.rf), {t0: 0, t1: 5, t2: -7})
+    #     expect_memory(go.inspect_mem(cpu.d_mem), {})
+
     # TODO: Can't get negative operands working.
     # https://piazza.com/class/l18i7acdkcy3un?cid=101
 
