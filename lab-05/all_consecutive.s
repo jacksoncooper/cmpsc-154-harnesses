71808e541080ce02c803f268b2b5a3fd81d82da8
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2022-05-09 13:02:22 -0700

    Add test for bad host to harness.

--- lab-05/all_consecutive.s
+++ lab-05/all_consecutive.s
@@ -2,26 +2,26 @@
 
 # Distinguishes the host in a network given:
 
-#  - the size of the subnet mask
-#  - an Internet address
+#  - The size of the subnet mask at 0($a3).
+#  - An Internet address at 4($a3).
 
-# Tells you if the host is number 42.
+# Tells you if the host is number 42 in ASCII in 8($a3).
 
 .text
 
 main:
-    add $a0, $zero, $zero        # For testing before loading to the CPU.
-    add $a1, $zero, $zero        # 1.2.3.4/16
+    # add $a0, $zero, $zero        # For testing before loading to the CPU.
+    # add $a1, $zero, $zero        # 1.2.3.4/16
 
-    addi $a0, $a0, 16
+    # addi $a0, $a0, 16
 
-    lui $a1, 0x1234
-    addi $a1, 0x002a
+    # lui $a1, 0x1234
+    # addi $a1, 0x002a
 
-    add $a3, $zero, $sp
+    # add $a3, $zero, $sp
 
-    sw $a0, 0($a3)
-    sw $a1, 4($a3)
+    # sw $a0, 0($a3)
+    # sw $a1, 4($a3)
 
     # ---
 
@@ -30,7 +30,7 @@ main:
     add $t1, $t1, $zero          # $t1 := The subnet mask to be made.
     add $t2, $zero, $zero        # $t2 := The fill flag.
     addi $t3, $zero, 32          # $t3 := The bitwidth of an IPv4 address.
-    add $t4, $zero, $zero        # $t4 := The fill flag.
+    add $t4, $zero, $zero        # $t4 := The constant 1.
     addi $t4, $t4, 1
 
 fill_ones:
@@ -47,7 +47,7 @@ done_fill_ones:
 lw $t5, 4($a3)                   # $t5 := The Internet address from memory.
 and $t6, $t1, $t5                # $t6 := The host number.
 
-addi $t7, $t7, 42
+addi $t7, $t7, 42                # t7 := The expected host.
 beq $t6, $t7, host_is_forty_two
 beq $zero, $zero, host_is_not_forty_two
 
@@ -63,7 +63,7 @@ host_is_not_forty_two:
     ori $t0, $t0, 0x6e6f
 
 write_ascii:
-    sw $t0, 8($a3)
+    sw $t0, 8($a3)               # t0 := Exhausted temporaries, use t0 for out.
 
 exit:
     beq $zero, $zero, exit
--- lab-05/harness.py
+++ lab-05/harness.py
@@ -4,7 +4,9 @@ import pyrtl as rtl
 
 import cpu
 
-v0, t0, t1, t2, t3, t4 = 2, 8, 9, 10, 11, 12
+v0 = 2
+a0, a1, a2, a3 = 4, 5, 6, 7
+t0, t1, t2, t3, t4, t5, t6, t7 = 8, 9, 10, 11, 12, 13, 14, 15
 
 def expect_memory(actual, expected):
     # The memory must have the same number of addresses with assigned values.
@@ -396,3 +398,36 @@ class TestConsecutiveInstructions:
             go.step({})
         
         expect_memory(go.inspect_mem(cpu.rf), {t0: 655360, t1: 65336, t2: 718696, t3: 1, t4: 1})
+
+    def test_all_consecutive_with_bad_host(self):
+        memory = {
+            cpu.rf: {a3: 1024},
+            cpu.d_mem: {1024: 16, 1028: 0x1234002b},
+            cpu.i_mem: {
+                0: 0x8ce80000, 1: 0x21080001, 2: 0x01204820, 3: 0x00005020,
+                4: 0x200b0020, 5: 0x00006020, 6: 0x218c0001, 7: 0x0168502a,
+                8: 0x114c0004, 9: 0x01294820, 10: 0x21290001, 11: 0x21080001,
+                12: 0x1000fffa, 13: 0x8ced0004, 14: 0x012d7024, 15: 0x21ef002a,
+                16: 0x11cf0001, 17: 0x10000004, 18: 0x00004020, 19: 0x3c086f79,
+                20: 0x35086573, 21: 0x10000003, 22: 0x00004020, 23: 0x3c086f68,
+                24: 0x35086e6f, 25: 0xace80008, 26: 0x1000ffff
+            }
+        }
+
+        go = rtl.Simulation(
+            register_value_map = {cpu.pc: 0},
+            memory_value_map = memory
+        )
+
+        for i in range(500):
+            go.step({})
+        
+        expect_memory(go.inspect_mem(cpu.rf), {
+            t0: 0x6f686e6f, t1: 0x0000ffff, t2: 1, t3: 32, t4: 1,
+            t5: 0x1234002b, t6: 43, t7: 42,
+            a3: 1024
+        })
+
+        expect_memory(go.inspect_mem(cpu.d_mem), {
+            1024: 16, 1028: 0x1234002b, 1032: 0x6f686e6f
+        })
