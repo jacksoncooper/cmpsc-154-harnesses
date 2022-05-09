4b1fbe0532ccc9f7da508ae0d8e2e31bf870d588
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2022-05-09 10:20:57 -0700

    Annotate `lui_test.s`.

--- lab-05/lui_test.s
+++ lab-05/lui_test.s
@@ -3,18 +3,18 @@
 .text
 
 main:
-    lui $t0, 10             #t0 = 655360
-    lui $t1, 1              #t1 = 65536
-    addi $t1, $t1, -200     #t1 = 65336
-    and $t2, $t2, $zero     #t2 = 0
-    and $t3, $t3, $zero     #t3 = 0
-    addi $t4, $zero, 1      #t4 = 1
+    lui $t0, 10             # t0 = 655360
+    lui $t1, 1              # t1 = 65536
+    addi $t1, $t1, -200     # t1 = 65336
+    and $t2, $t2, $zero     # t2 = 0
+    and $t3, $t3, $zero     # t3 = 0
+    addi $t4, $zero, 1      # t4 = 1
 
 loop:
     beq $t3, $t4, exit
 
-    add $t2, $t2, $t1       #t2 = t2 + t1
-    slt $t3, $t0, $t2
+    add $t2, $t2, $t1       # t2 = t2 + t1 (65336)
+    slt $t3, $t0, $t2       # exit if $t0 (655360) < $t2
 
     beq $zero, $zero, loop
 
